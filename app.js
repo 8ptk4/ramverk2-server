2ec0d2261369007c2679dceb050c06010ac67dcd
8ptk4 <putte.karlsson@gmail.com>
2019-09-17 15:06:47 +0200

    changed port to 8080

--- app.js
+++ app.js
@@ -1,11 +1,15 @@
 const express = require("express");
 const app = express();
 
-const port = 1337;
+const port = 8080;
 
 // Add a route
 app.get("/", (req, res) => {
     res.send("Hello World");
+});
+
+app.get("/test", (req, res) => {
+    res.send("Testar lite");
 });
 
 // Start up server
